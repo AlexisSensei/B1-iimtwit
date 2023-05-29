971b9f94be601816f241e0813b8b09da84d2188d
ProfCitron <jeveuxparler@profcitron.fr>
2023-05-29 22:41:54 +0200

    twit card

--- index.php
+++ index.php
@@ -13,6 +13,7 @@ $tags = $tags->fetchAll(PDO::FETCH_ASSOC);
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>IIMTWIT</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" integrity="sha512-iecdLmaskl7CVkqkXNQ/ZH/XLlvWZOJyj7Yy7tcenmpD1ypASozpmT/E0iPtmFIB46ZmdtAc9eNBvH0H/ZpiBw==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <link rel="stylesheet" href="css/style.css">
 </head>
 <body>
@@ -20,8 +21,8 @@ $tags = $tags->fetchAll(PDO::FETCH_ASSOC);
         <header class="header">
 
         </header>
-        <main>
-
+        <main class="feed">
+            <?php require 'tpl/loop/single-twit.php'; ?>
         </main>
         <aside>
             <div class="tags-wrapper">
@@ -33,6 +34,7 @@ $tags = $tags->fetchAll(PDO::FETCH_ASSOC);
     </div>
     
     <footer></footer>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/js/all.min.js" integrity="sha512-fD9DI5bZwQxOi7MhYWnnNPlvXdp/2Pj3XSTRrFs5FQa4mizyGLnJcN6tuvUS6LbmgN1ut+XGSABKvjN0H6Aoow==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="js/main.js"></script>
 </body>
 </html>
---
+++ tpl/loop/single-twit.php
@@ -0,0 +1,15 @@
+<div class="twit">
+    <div>
+        <div class="twit__subtitle">
+            Coding
+        </div>
+        <div class="twit__content">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Laborum, tempora distinctio! Consectetur natus itaque molestiae doloremque dolores eos nostrum nobis.
+        </div>
+        <div class="twit__user">
+            <img src="images/user.png" alt="Photo de profil">
+            <p>Alexis B.</p>
+        </div>
+    </div>
+    <a href="" class="button--remove"><i class="fa-solid fa-trash"></i> Supprimer</a>
+</div>
