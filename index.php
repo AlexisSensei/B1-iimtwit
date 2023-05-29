d1692647bc26e6157204bf9569d83baae738b7c1
ProfCitron <jeveuxparler@profcitron.fr>
2023-05-29 22:20:25 +0200

    Aside navigation with filter buttons

--- index.php
+++ index.php
@@ -1,2 +1,38 @@
-<?php
-echo 'cc';
+<?php 
+require 'config/database.php'; 
+
+$tags = $db->prepare('SELECT tag_name as name, tag_slug as slug, tag_color as color FROM tags ORDER BY tag_name ASC');
+$tags->execute();
+$tags = $tags->fetchAll(PDO::FETCH_ASSOC);
+
+?>
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>IIMTWIT</title>
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    <div class="main-layout">
+        <header class="header">
+
+        </header>
+        <main>
+
+        </main>
+        <aside>
+            <div class="tags-wrapper">
+                <?php foreach($tags as $tag) { ?>
+                    <button class="tag-button <?= $tag['slug']; ?>" style="--tag-color: #<?= $tag['color']; ?>"><?= $tag['name']; ?></button>
+                <?php } ?>
+            </div>
+        </aside>
+    </div>
+    
+    <footer></footer>
+    <script src="js/main.js"></script>
+</body>
+</html>
